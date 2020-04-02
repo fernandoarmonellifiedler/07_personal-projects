6542a2d583e43497a17bf6de6c862c887a926449
fernandoarmonellifiedler <62020070+fernandoarmonelli@users.noreply.github.com>
2020-04-02 20:31:12 -0300

    Subido proyecto con seleccion automatica
    Primeros comandos

---
+++ Tirada de Tarot - Automatic/Tirada de Tarot/script.js
@@ -0,0 +1,27 @@
+/*
+1_ funcion para tomar 3 cartas entre 0 y 21
+2_ asignar imagen y arcano a esas 3 cartas
+let carta1 = funtion.getCartas(min , max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor
+}
+let carta2 = funtion.getCartas(min , max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor
+}
+let carta3 = funtion.getCartas(min , max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor
+*/
+let max = 21
+let min = 0
+
+function getCartas(min , max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.round()
+}
+
+
+
+
+console.log(getCartas())
