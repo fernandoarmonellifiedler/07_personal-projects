bf26a3cdf9afb658955e095d73fca041b6a71c96
fernandoarmonellifiedler <62020070+fernandoarmonelli@users.noreply.github.com>
2020-04-03 17:57:18 -0300

    Info clarified

--- Tirada de Tarot - Automatic/Tirada de Tarot/script.js
+++ Tirada de Tarot - Automatic/Tirada de Tarot/script.js
@@ -1,30 +1,20 @@
-/*
-1_ funcion para tomar 3 cartas entre 0 y 21
-2_ asignar imagen y arcano a esas 3 cartas
-
-
-function getCartas(min , max) {
-    const valor = Math.random() * (max - min) + min
-    return Math.round()
-}
-
-carta[i] = getCarta(0,21)
-*/
 var carta = []
 
-
+// function para seleccionar un valor random
 function getCarta(min , max) {
     const valor = Math.random() * (max - min) + min
     return Math.round(valor)   
 }
 
+//function para asignar valores al array carta[0, 1, 2]
 function inicio() {
     for (var i = 0 ; i <= 2 ; i++)
         carta[i] = getCarta(0,21)
         return carta[i]   
 }
- 
+
 inicio()
 console.log(carta[0])
 console.log(carta[1])
 console.log(carta[2])
+ 
