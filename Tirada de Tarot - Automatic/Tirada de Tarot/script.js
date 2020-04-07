f83980f9f7d598f8aa0fb18d8342a168e7f59538
fernandoarmonellifiedler <62020070+fernandoarmonelli@users.noreply.github.com>
2020-04-07 11:31:55 -0300

    Algunos cambios

--- Tirada de Tarot - Automatic/Tirada de Tarot/script.js
+++ Tirada de Tarot - Automatic/Tirada de Tarot/script.js
@@ -1,4 +1,6 @@
-var carta = []
+let carta = []
+let arc = []
+//var img = []
 
 // function para seleccionar un valor random
 function getCarta(min , max) {
@@ -8,13 +10,27 @@ function getCarta(min , max) {
 
 //function para asignar valores al array carta[0, 1, 2]
 function inicio() {
-    for (var i = 0 ; i <= 2 ; i++)
+    for (let i = 0 ; i <= 2 ; i++) 
         carta[i] = getCarta(0,21)
-        return carta[i]   
+        return carta[i]           
 }
 
+function asignarArcano() {
+    for (let i = 0 ; i <= 21 ; i++) {
+        arc[i] = document.createElement("img").src = `cartas/arcano_${i}.jpg`
+        console.log(arc[i])
+    }
+}
+
+/*
 inicio()
 console.log(carta[0])
 console.log(carta[1])
 console.log(carta[2])
- 
+
+asignarArcano()
+
+console.log(arc[4])
+*/
+// document.getElementById("image").src = "pic_mountain.jpg";
+//img[i].url=`cartas/arcano_${i}.jpg`
