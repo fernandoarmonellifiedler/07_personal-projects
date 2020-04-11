dea89c2bafa5a52dc010012033b16a4b9f4630ab
fernandoarmonellifiedler <62020070+fernandoarmonelli@users.noreply.github.com>
2020-04-11 15:48:32 -0300

    Primer logro!
    la tirada funciona pero hay que escribir codigo para evitar repeticiones y arreglar el visual en css

--- Tirada de Tarot - Automatic/Tirada de Tarot/_js/script.js
+++ Tirada de Tarot - Automatic/Tirada de Tarot/_js/script.js
@@ -1,7 +1,5 @@
 let carta = []
 let arc = []
-//var img = []
-
 // function para seleccionar un valor random
 function getCarta(min , max) {
     const valor = Math.random() * (max - min) + min
@@ -12,34 +10,24 @@ function getCarta(min , max) {
 function inicio() {
     for (let i = 0 ; i <= 2 ; i++) 
         carta[i] = getCarta(0,21)
-        return carta[i]           
+        //return carta[i]           
 }
-
-function asignarArcano() {
-    for (let i = 0 ; i <= 21 ; i++) {
-        arc[i] = document.createElement("img").src = `cartas/arcano_${i}.jpg`
-        console.log(arc[i])
-    }
-}
-
-/* sugestão feita por tiago
-const img1 = document.createElement('img')
-img1.setAttribute('class', 'foto')
-const img2 = document.createElement('img')
-img2.setAttribute('class', 'foto')
-const img3 = document.createElement('img')
-img3.setAttribute('class', 'foto')
 
 function tirada() {
-	if (valores.length <= 2) {
-		window.alert("Agrega 3 cartas para realizar la tirada")
-	} else {
-		img1.setAttribute('src', `cartas/arcano_${valores[0]}.jpg`);
-		document.getElementById('res').appendChild(img1);
-		img2.setAttribute('src', `cartas/arcano_${valores[1]}.jpg`);
-		document.getElementById('res').appendChild(img2);
-		img3.setAttribute('src', `cartas/arcano_${valores[2]}.jpg`);
-		document.getElementById('res').appendChild(img3);
-	}
-});
+	
+	res.innerHTML = ''
+	const img1 = document.createElement('img')
+	img1.setAttribute('class', 'foto')
+	const img2 = document.createElement('img')
+	img2.setAttribute('class', 'foto')
+	const img3 = document.createElement('img')
+	img3.setAttribute('class', 'foto')
+	inicio()
+	img1.setAttribute('src', `_img/arcano_${carta[0]}.jpg`);
+	document.getElementById('res').appendChild(img1);
+	img2.setAttribute('src', `_img/arcano_${carta[1]}.jpg`);
+	document.getElementById('res').appendChild(img2);
+	img3.setAttribute('src', `_img/arcano_${carta[2]}.jpg`);
+	document.getElementById('res').appendChild(img3);
+
 }
