08a58bc0ffee9a799bcd8e5caf60471b8adf4055
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-02-09 20:55:34 -0300

    correct img src url

--- react/my-projects-netlify/my-app/src/data.js
+++ react/my-projects-netlify/my-app/src/data.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 1,
     img:
-      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/public/img_1.png',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_1.png',
     name: 'javascript-tirada-de-tarot',
     link:
       'https://javascript-tirada-de-tarot.netlify.app',
@@ -10,94 +10,57 @@ export default [
   {
     id: 2,
     img:
-      'https://github.com/fernandoarmonellifiedler/personal-projects/blob/master/react/my-projects-netlify/my-app/public/img_2.png?raw=true',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_2.png',
     name: 'javascript-todo-app-list',
     link: 'https://javascript-todo-app-list.netlify.app',
   },
   {
     id: 3,
     img:
-      'https://github.com/fernandoarmonellifiedler/personal-projects/blob/master/react/my-projects-netlify/my-app/public/img_3.png?raw=true',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_3.png',
     name: 'javascript-carrito-de-compras',
     link: 'https://javascript-carrito-de-compras.netlify.app',
   },
   {
     id: 4,
     img:
-      'https://github.com/fernandoarmonellifiedler/personal-projects/blob/master/react/my-projects-netlify/my-app/public/img_4.png?raw=true',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_4.png',
     name: 'web-development-living-the-simple-life',
     link: 'https://web-development-living-the-simple-life.netlify.app',
   },
   {
     id: 5,
     img:
-      'https://github.com/fernandoarmonellifiedler/personal-projects/blob/master/react/my-projects-netlify/my-app/public/img_5.png?raw=true',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_5.png',
     name: 'web-development-my-portfolio',
     link: 'https://web-development-my-portfolio.netlify.app',
   },
   {
     id: 6,
     img:
-      'https://github.com/fernandoarmonellifiedler/personal-projects/blob/master/react/my-projects-netlify/my-app/public/img_6.png?raw=true',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_6.png',
     name: 'react-tic-tac-toe-gameapp',
     link: 'https://react-tic-tac-toe-gameapp.netlify.app',
   },
   {
     id: 7,
     img:
-      'https://github.com/fernandoarmonellifiedler/personal-projects/blob/master/react/my-projects-netlify/my-app/public/img_7.png?raw=true',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_7.png',
     name: 'react-array-in-component',
     link: 'https://react-array-in-component.netlify.app',
   },
   {
     id: 8,
     img:
-      'https://github.com/fernandoarmonellifiedler/personal-projects/blob/master/react/my-projects-netlify/my-app/public/img_8.png?raw=true',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_8.png',
     name: 'react-gradebook',
     link: 'https://react-gradebook.netlify.app',
   },
   {
     id: 9,
     img:
-      'https://github.com/fernandoarmonellifiedler/personal-projects/blob/master/react/my-projects-netlify/my-app/public/img_9.png?raw=true',
+      'https://raw.githubusercontent.com/fernandoarmonellifiedler/personal-projects/master/react/my-projects-netlify/my-app/src/images/img_9.png',
     name: 'react-bookslist',
     link: 'https://react-bookslist.netlify.app',
   },
 ];
-// export default [
-//   {
-//     id: 1,
-//     name: 'Bertie Yates',
-//     age: 29,
-//     image:
-//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
-//   },
-//   {
-//     id: 2,
-//     name: 'Hester Hogan',
-//     age: 32,
-//     image:
-//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
-//   },
-//   {
-//     id: 3,
-//     name: 'Larry Little',
-//     age: 36,
-//     image:
-//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-//   },
-//   {
-//     id: 4,
-//     name: 'Sean Walsh',
-//     age: 34,
-//     image:
-//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-//   },
-//   {
-//     id: 5,
-//     name: 'Lola Gardner',
-//     age: 29,
-//     image:
-//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-//   },
-// ];
